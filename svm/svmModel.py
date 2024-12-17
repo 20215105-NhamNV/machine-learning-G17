21f363b80b8b54a1b3bcdec6e8b55cc39dfe94f6
TranHuyHoangIT <tranhuyhoangk14a1@gmail.com>
2024-12-17 18:36:02 +0700

    Hoang add accuracy

--- svm/svmModel.py
+++ svm/svmModel.py
@@ -14,39 +14,31 @@ class Kernel(object):
 
     @staticmethod
     def linear():
-        # Implementing the linear method relation between two features x and y
-        return lambda x,y:np.dot(x.T,y)
+        return lambda x, y: np.dot(x.T, y)
 
     @staticmethod
     def polykernel(dimension, offset):
-        return lambda x, y: ((offset + np.dot(x.T,y)) ** dimension)
+        return lambda x, y: ((offset + np.dot(x.T, y)) ** dimension)
 
     @staticmethod
     def radial_basis(gamma):
-        return lambda x, y: np.exp(-gamma*la.norm(np.subtract(x, y)))
+        return lambda x, y: np.exp(-gamma * la.norm(np.subtract(x, y)))
 
 class SVMTrainer(object):
 
     def __init__(self, kernel, c):
-        # Assigning the attributes kernal and C value
         self.kernel = kernel
         self.c = c
 
     def train(self, X, y):
-        # Training function
-        # Caluculate the langrange multipliers
         lagrange_multipliers = self.compute_multipliers(X, y)
-        # Trainer returns SVM predictor that is used to predict which class the test element belongs to
         return self.construct_predictor(X, y, lagrange_multipliers)
 
     def kernel_matrix(self, X, n_samples):
-        # Size of kernal matrix is (no_of_inputs , no_of_inputs)
-        # Reason for this is that kernel function value is calculated between every 2 inputs given
         K = np.zeros((n_samples, n_samples))
         for i, x_i in enumerate(X):
             for j, x_j in enumerate(X):
                 K[i, j] = self.kernel(x_i, x_j)
-        #  Returns the kernel function values
         return K
 
     def construct_predictor(self, X, y, lagrange_multipliers):
@@ -70,114 +62,96 @@ class SVMTrainer(object):
             support_vector_labels=support_vector_labels)
 
     def compute_multipliers(self, X, y):
-        # n_samples is no_of_inputs
-        # n_features is no_of_features
         n_samples, n_features = X.shape
-        # Returns kernel function matrix
-        K = self.kernel_matrix(X,n_samples)
-        # np.outer(a,b) gives
+        K = self.kernel_matrix(X, n_samples)
         P = cvxopt.matrix(np.outer(y, y) * K)
         q = cvxopt.matrix(-1 * np.ones(n_samples))
-        # Create a diagonal matrix of (n_samples , n_samples) dimension with -1 as value
         G_std = cvxopt.matrix(np.diag(np.ones(n_samples) * -1))
         h_std = cvxopt.matrix(np.zeros(n_samples))
         G_slack = cvxopt.matrix(np.diag(np.ones(n_samples)))
         h_slack = cvxopt.matrix(np.ones(n_samples) * self.c)
         G = cvxopt.matrix(np.vstack((G_std, G_slack)))
         h = cvxopt.matrix(np.vstack((h_std, h_slack)))
-        A = cvxopt.matrix(y, (1, n_samples) , 'd')
+        A = cvxopt.matrix(y, (1, n_samples), 'd')
         b = cvxopt.matrix(0.0)
         solution = cvxopt.solvers.qp(P, q, G, h, A, b)
-        # To flatten as one dimension array
         return np.ravel(solution['x'])
 
 class SVMPredictor(object):
 
-    # Initializing the SVM predictor
-    # Needs kernel (K) , bias (b) , weights (w) , support_vectors and support_vector_labels
-    def __init__(self,
-                 kernel,
-                 bias,
-                 weights,
-                 support_vectors,
-                 support_vector_labels):
+    def __init__(self, kernel, bias, weights, support_vectors, support_vector_labels):
         self._kernel = kernel
         self._bias = bias
         self._weights = weights
         self._support_vectors = support_vectors
         self._support_vector_labels = support_vector_labels
         assert len(support_vectors) == len(support_vector_labels)
-        # Weights is equal to support vector labels as per mathematical formulation
         assert len(weights) == len(support_vector_labels)
 
     def predict(self, x):
-        # result = b
         result = self._bias
-        for z_i, x_i, y_i in zip(self._weights,
-                                 self._support_vectors,
-                                 self._support_vector_labels):
-            # result += w * support_vector_labels * K
+        for z_i, x_i, y_i in zip(self._weights, self._support_vectors, self._support_vector_labels):
             result += z_i * y_i * self._kernel(x_i, x)
-        # Returning the sign of the value predicted
-        # +1 means belonging to positive (non spam) class
-        # -1 means belonging to negative (spam) class
         return np.sign(result).item()
 
-def calculate(true_positive,false_positive,false_negative,true_negative):
+def calculate(true_positive, false_positive, false_negative, true_negative):
     result = {}
     result['precision'] = true_positive / (true_positive + false_positive)
     result['recall'] = true_positive / (true_positive + false_negative)
+    result['accuracy'] = (true_positive + true_negative) / (true_positive + false_positive + false_negative + true_negative)
     return result
 
-def confusion_matrix(true_positive,false_positive,false_negative,true_negative):
-    matrix = PrettyTable([' ', 'Ham' , 'Spam'])
-    matrix.add_row(['Ham', true_positive , false_positive])
-    matrix.add_row(['Spam', false_negative , true_negative])
-    return matrix , calculate(true_positive,false_positive,false_negative,true_negative)
+def confusion_matrix(true_positive, false_positive, false_negative, true_negative):
+    matrix = PrettyTable([' ', 'Ham', 'Spam'])
+    matrix.add_row(['Ham', true_positive, false_positive])
+    matrix.add_row(['Spam', false_negative, true_negative])
+    return matrix, calculate(true_positive, false_positive, false_negative, true_negative)
 
-def implementSVM(X_train,Y_train,X_test,Y_test,parameters,type):
+def implementSVM(X_train, Y_train, X_test, Y_test, parameters, type):
     ham_spam = 0
     spam_spam = 0
     ham_ham = 0
     spam_ham = 0
-    if(type=="polykernel"):
+    if (type == "polykernel"):
         dimension = parameters['dimension']
         offset = parameters['offset']
-        trainer = SVMTrainer(Kernel.polykernel(dimension,offset),1)
-        predictor = trainer.train(X_train,Y_train)
-    elif(type=="linear"):
-        trainer = SVMTrainer(Kernel.linear(),1)
-        predictor = trainer.train(X_train,Y_train)
+        trainer = SVMTrainer(Kernel.polykernel(dimension, offset), 1)
+        predictor = trainer.train(X_train, Y_train)
+    elif (type == "linear"):
+        trainer = SVMTrainer(Kernel.linear(), 1)
+        predictor = trainer.train(X_train, Y_train)
     for i in range(X_test.shape[0]):
         ans = predictor.predict(X_test[i])
-        if(ans==-1 and Y_test[i]==-1):
-            spam_spam+=1
-        elif(ans==1 and Y_test[i]==-1):
-            spam_ham+=1
-        elif(ans==1 and Y_test[i]==1):
-            ham_ham+=1
-        elif(ans==-1 and Y_test[i]==1):
-            ham_spam+=1
-    return confusion_matrix(ham_ham,ham_spam,spam_ham,spam_spam)
+        if (ans == -1 and Y_test[i] == -1):
+            spam_spam += 1
+        elif (ans == 1 and Y_test[i] == -1):
+            spam_ham += 1
+        elif (ans == 1 and Y_test[i] == 1):
+            ham_ham += 1
+        elif (ans == -1 and Y_test[i] == 1):
+            ham_spam += 1
+    return confusion_matrix(ham_ham, ham_spam, spam_ham, spam_spam)
 
-def write_to_file(matrix,result,parameters,type,start_time):
-    f = open("results1.txt","a")
-    if(type=="polykernel"):
+def write_to_file(matrix, result, parameters, type, start_time):
+    f = open("results1.txt", "a")
+    if (type == "polykernel"):
         f.write("Polykernel model parameters")
         f.write("\n")
         f.write("Dimension : " + str(parameters['dimension']))
         f.write("\n")
         f.write("Offset : " + str(parameters['offset']))
-    elif(type=="linear"):
+    elif (type == "linear"):
         f.write("Linear model")
     f.write("\n")
     f.write(matrix.get_string())
     f.write("\n")
-    f.write("Precision : " + str(round(result['precision'],2)))
+    f.write("Precision : " + str(round(result['precision'], 2)))
     f.write("\n")
-    f.write("Recall : " + str(round(result['recall'],2)))
+    f.write("Recall : " + str(round(result['recall'], 2)))
     f.write("\n")
-    f.write("Time spent for model : " + str(round(time()-start_time,2)))
+    f.write("Accuracy : " + str(round(result['accuracy'], 2)))
+    f.write("\n")
+    f.write("Time spent for model : " + str(round(time() - start_time, 2)))
     f.write("\n")
     f.write("\n")
     f.write("\n")
@@ -187,23 +161,22 @@ global_start_time = time()
 
 cvxopt.solvers.options['show_progress'] = False
 
-df1 = pd.read_csv('frequency1.csv',header=0)
-
-input_output =  df1.to_numpy()
-X = input_output[:,:-1]
-Y = input_output[:,-1:]
+df1 = pd.read_csv('frequency1.csv', header=0)
+input_output = df1.to_numpy()
+X = input_output[:, :-1]
+Y = input_output[:, -1:]
 
 total = X.shape[0]
 train = int(X.shape[0] * 70 / 100)
 
-X_train = X[:train,:]
-Y_train = Y[:train,:]
-X_test = X[train:,:]
-Y_test = Y[train:,:]
+X_train = X[:train, :]
+Y_train = Y[:train, :]
+X_test = X[train:, :]
+Y_test = Y[train:, :]
 
-f = open("results1.txt","w+")
+f = open("results1.txt", "w+")
 f.close()
-k=0
+k = 0
 
 type = {}
 parameters = {}
@@ -212,29 +185,29 @@ type['1'] = "polykernel"
 type['2'] = "linear"
 
 print("Menu")
-print("1.svm tuyến tính")
-print("2.svm phi tuyến")
-choose = int(input("Nhập lựa chọn(1 hoặc 2):"))
+print("1. svm tuyến tính")
+print("2. svm phi tuyến")
+choose = int(input("Nhập lựa chọn (1 hoặc 2): "))
 if (choose == 1):
     print("Đang xử lí...")
     start_time = time()
-    matrix , result = implementSVM(X_train,Y_train,X_test,Y_test,parameters,str(type['2']))
-    write_to_file(matrix,result,parameters,type,start_time)
-    k+=1
+    matrix, result = implementSVM(X_train, Y_train, X_test, Y_test, parameters, str(type['2']))
+    write_to_file(matrix, result, parameters, type, start_time)
+    k += 1
     print("Done : " + str(k))
 
-if(choose == 2):
+if (choose == 2):
     print("Đang xử lí...")
-    i=2
-    j=1
+    i = 2
+    j = 1
     start_time = time()
     parameters['dimension'] = i
     parameters['offset'] = j
-    matrix , result = implementSVM(X_train,Y_train,X_test,Y_test,parameters,str(type['1']))
-    write_to_file(matrix,result,parameters,type,start_time)
-    k+=1
+    matrix, result = implementSVM(X_train, Y_train, X_test, Y_test, parameters, str(type['1']))
+    write_to_file(matrix, result, parameters, type, start_time)
+    k += 1
     print("Done : " + str(k))
 
-f = open("results1.txt","a")
-f.write("Time spent for entire code : " + str(round(time()-global_start_time,2)))
+f = open("results1.txt", "a")
+f.write("Time spent for entire code : " + str(round(time() - global_start_time, 2)))
 f.close()
